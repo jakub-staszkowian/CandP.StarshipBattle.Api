baa300d320ceffda88b830f13f73beca3c737fe7
Kuba <jakubstaszkowian@gmail.com>
2020-02-28 20:23:06 +0100

    add starshipdto and mapping extension methods

---
+++ StarshipBattle/StarshipBattle.Logic/Extensions/StarshipExtenisons.cs
@@ -0,0 +1,33 @@
+﻿using StarshipBattle.Data.Entites;
+using StarshipBattle.Logic.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarshipBattle.Logic.Extensions
+{
+    public static class StarshipExtenisons
+    {
+        public static StarshipDto ToDto(this Starship starship)
+        {
+            return new StarshipDto
+            {
+                Id = starship.Id,
+                CrewQuantity = starship.CrewQuantity,
+                ImageUrl = starship.ImageUrl,
+                Name = starship.Name
+            };
+        }
+
+        public static Starship ToEntity(this StarshipDto starship)
+        {
+            return new Starship
+            {
+                Id = starship.Id,
+                CrewQuantity = starship.CrewQuantity,
+                ImageUrl = starship.ImageUrl,
+                Name = starship.Name
+            };
+        }
+    }
+}

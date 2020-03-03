c07791b9cc14bc9c870364863f1cc61284c12841
Kuba <jakubstaszkowian@gmail.com>
2020-03-03 19:16:52 +0100

    add swagger

---
+++ StarshipBattle/StarshipBattle.Api/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace StarshipBattle.Api.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "StarshipBattleAPI", Version = "v1" });
+            });
+        }
+
+        public static void ConfigureSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarshipBattleAPI v1");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}

b0cb3cd034cee7be32597c08de7be868a3ebd166
Kuba <jakubstaszkowian@gmail.com>
2020-03-03 18:59:05 +0100

    add validators

---
+++ StarshipBattle/StarshipBattle.Logic/Validation/RemoveStarshipValidator.cs
@@ -0,0 +1,13 @@
+﻿using StarshipBattle.Data.Entites;
+using StarshipBattle.Logic.Validation.Interfaces;
+
+namespace StarshipBattle.Logic.Validation
+{
+    public class RemoveStarshipValidator : Validator<Starship>, IRemoveStarshipValidator
+    {
+        public override void Validate(Starship model)
+        {
+            ValidateCase(model, m => m != null, "Not added Starship cannot have id"); ;
+        }
+    }
+}
---
+++ StarshipBattle/StarshipBattle.Logic/Validation/Validator[T].cs
@@ -0,0 +1,19 @@
+﻿using StarshipBattle.Logic.Validation.Exceptions;
+using StarshipBattle.Logic.Validation.Interfaces;
+using System;
+
+namespace StarshipBattle.Logic.Validation
+{
+    public  abstract class Validator<TModel> : IValidator<TModel>
+    {
+        public abstract void Validate(TModel model);
+
+        protected void ValidateCase(TModel model, Func<TModel, bool> test, string errorMessage)
+        {
+            if (!test(model))
+            {
+                throw new ValidationException(errorMessage);
+            }
+        }
+    }
+}

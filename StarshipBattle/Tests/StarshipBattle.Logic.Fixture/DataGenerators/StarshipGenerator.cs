6f7cb3a5cdb853cf188495ac82040bfe84b03de1
Kuba <jakubstaszkowian@gmail.com>
2020-03-07 13:49:11 +0100

    fix bugs, add missing unit tests

---
+++ StarshipBattle/Tests/StarshipBattle.Logic.Fixture/DataGenerators/StarshipGenerator.cs
@@ -0,0 +1,23 @@
+﻿using StarshipBattle.Data.Entites;
+
+namespace StarshipBattle.Logic.Fixture.DataGenerators
+{
+    public class StarshipGenerator
+    {
+        public static Starship GetNull()
+        {
+            return null;
+        }
+
+        public static Starship GetValid()
+        {
+            return new Starship
+            {
+                Id = 1,
+                CrewQuantity = 4,
+                ImageUrl = "www.google.com",
+                Name = "Name"
+            };
+        }
+    }
+}
